1ca4eaeb1c02e9a86d1611661e08c740acbea5c5
EfratSharabi <Efratz0879@gmail.com>
2025-12-24 18:10:36 +0200

    added ServeStaticModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
@@ -11,6 +13,9 @@ import { ChatModule } from './chat/chat.module';
       isGlobal: true,
     }),
     EventEmitterModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'web', 'browser'),
+    }),
     ChatModule
   ],
   controllers: [AppController],
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,9 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { Request, Response } from 'express';
-import { join } from 'path';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
@@ -17,15 +14,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // Serve Angular SPA
-  const publicPath = join(__dirname, 'public');
-  app.useStaticAssets(publicPath);
-  app.setBaseViewsDir(publicPath);
-
-  // Use Express types for req/res
-  app.getHttpAdapter().get('/:path(.*)', (req: Request, res: Response) => {
-    res.sendFile(join(publicPath, 'index.html'));
-  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
